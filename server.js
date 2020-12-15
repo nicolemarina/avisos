53fc7945fc64354f9cc7ec6823819fad3456b65d
Nicole Marina <nicolemarina438@gmail.com>
2020-12-14 23:25:09 -0300

    estrutura layout

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express') 
 const bodyParser = require('body-parser')
 
+//rotas do aviso
+const routerAvisos = require('./components/avisos/avisosController')
+
 const app = express() //iniciar express
 
 app.set('view engine', 'ejs') //configura engine
@@ -10,9 +13,7 @@ app.use(bodyParser.urlencoded({extended:false})) // configura bodyParses
 app.use(bodyParser.json())
 
 //rotas
-app.get("/", (req,res)=>{
-    res.send("teste")
-})
+app.use(routerAvisos)
 
 //configuração da porta
 app.listen(3000)
